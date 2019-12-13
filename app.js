1adfd6b168c0960252ae629dfc0b807a66dbf39f
David Pang <dpang2@uci.edu>
2019-12-13 13:34:04 -0800

    added switch statement

--- app.js
+++ app.js
@@ -8,11 +8,36 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
 var today = new Date();
 var currentDay = today.getDay()
-if(currentDay === 6 || currentDay === 0){
-    res.sendfile(__dirname + '/weekend.html')
-} else {
-    res.sendfile(__dirname + '/weekday.html')
+var day = '';
+
+switch (currentDay) {
+        case 0:
+        day ='Sunday';
+        break;
+        case 1:
+        day ='Monday';
+        break;
+        case 2:
+        day ='Tuesday';
+        break;
+        case 3:
+        day ='Wednesday';
+        break;
+        case 4:
+        day ='Thursday';
+        break;
+        case 5:
+        day ='Friday';
+        break;
+        case 6:
+        day ='Saturday';
+        break;
+
+    default:
+        console.log('Error: current day is equal to:' + currentDay)
+        break;
 }
+res.render('list', {kindOfDay: day});
 });
 
 app.listen(3000, function(){
